3c18b62adec6007109c29a55a38c55bfe36c4523
Eren Küçük <erenkucuk2002@gmail.com>
2023-04-18 23:05:41 +0300

    Add Bodycomp.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Bodycomp from "./components/Bodycomp";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <div>
         <Header />
+        <Bodycomp />
         <Navbar />
       </div>
     </>
---
+++ src/components/Bodycomp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bodycomp = () => {
+  return <div>Bodycomp</div>;
+};
+
+export default Bodycomp;
