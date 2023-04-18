3d30237e8b2e81b7037f40dfe235da83e2a79d0a
Eren Küçük <erenkucuk2002@gmail.com>
2023-04-18 23:17:47 +0300

    Swipper Demo

--- src/components/Bodycomp.js
+++ src/components/Bodycomp.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Container, Row, Col } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Bodycomp = () => {
   return (
     <Container>
       <Row>
-        <Col>//Carusel</Col>
+        <Col>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+          </Swiper>
+        </Col>
       </Row>
     </Container>
   );
